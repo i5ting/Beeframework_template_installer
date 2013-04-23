0bb3148a995c2a35ef9bcb3013fad9c6ce3a42d0
i5ting <i5ting@126.com>
2013-04-23 18:05:09 +0800

    use TemplateInfo.plist gem

---
+++ templates/batch.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby -wKU
+# encoding: UTF-8
+
+# gem install TemplateInfo
+`rvm use 1.9.3@rails320`
+
+`./mybuild.sh`
+
+Dir.foreach('./mybuild') do |f|  
+  if /lib/ =~ f
+    p f
+    #`cd #{f} &&ruby t.rb && cp -f TemplateInfo.xml #{dest_dir}/#{f}/TemplateInfo.plist &&cp -f TemplateInfo.xml #{dest_dir}/#{f}/TemplateInfo.plist`
+    `cd ./mybuild/#{f} && tif_create && cp -f TemplateInfo.xml ../../Xcode4_templates/#{f}/TemplateInfo1.plist `
+  end
+end  
