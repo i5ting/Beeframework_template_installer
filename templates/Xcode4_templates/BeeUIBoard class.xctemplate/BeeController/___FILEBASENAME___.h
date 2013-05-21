2df89db66a9136afffb1d282ccb1a95da96a1dce
i5ting <i5ting@126.com>
2013-05-21 14:47:11 +0800

    add classtemp.sh

---
+++ templates/Xcode4_templates/BeeUIBoard class.xctemplate/BeeController/___FILEBASENAME___.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "Bee_Controller.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : BeeController 
+
+AS_MESSAGE( LOCAL )
+AS_MESSAGE( REMOTE )
+
+@end
---
+++ templates/classtmpl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'beeframework-ios template installer'
+
+mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/beeframework-ios\ v0.x 
+
+
+cp -rf Xcode4_templates/BeeUIBoard\ class.xctemplate ~/Library/Developer/Xcode/Templates/File\ Templates/beeframework-ios\ v0.x 
